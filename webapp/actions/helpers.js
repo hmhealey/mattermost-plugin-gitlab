dd75db60d26b0fe860dfe58ea3695ae062e05d5e
Harrison Healey <harrisonmhealey@gmail.com>
2018-08-18 11:25:07 -0400

    Add missing files

---
+++ webapp/actions/helpers.js
@@ -0,0 +1,34 @@
+function bindClientFunc(clientFunc, success) {
+    return async (dispatch) => {
+        let data;
+        try {
+            data = await clientFunc;
+        } catch (error) {
+            return {error};
+        }
+
+        let connected = dispatch(checkAndHandleNotConnected(data));
+        if (!connected) {
+            return {error: data};
+        }
+
+        dispatch({
+            type: success,
+            data,
+        });
+
+        return {data};
+    };
+}
+
+function checkAndHandleNotConnected(data) {
+    return (dispatch) => {
+        if (!data || data.id !== 'not_connected') {
+            return true;
+        }
+
+        dispatch({type: ActionTypes.RECEIVED_DISCONNECTED});
+
+        return false;
+    };
+}
---
+++ webapp/components/sidebar_button.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {Tooltip, OverlayTrigger} from 'react-bootstrap';
+
+export default class SidebarButton extends React.PureComponent {
+	static propTypes = {
+		count: PropTypes.number,
+		href: PropTypes.string,
+		icon: PropTypes.node.isRequired,
+		onClick: PropTypes.func,
+		style: PropTypes.object,
+		tooltipId: PropTypes.string,
+		tooltipPlacement: PropTypes.string,
+		tooltipText: PropTypes.string,
+	};
+
+	render() {
+		let href = this.props.href;
+		if (href == null && this.props.onClick) {
+			href = '#';
+		}
+
+		let buttonText;
+		if (this.props.count) {
+			buttonText = ' ' + this.props.count;
+		}
+
+		let button = (
+			<a
+                href={href}
+                target='_blank'
+                style={this.props.style}
+            >
+            	{this.props.icon}
+                {buttonText}
+            </a>
+        );
+
+        if (this.props.tooltipText) {
+        	button = (
+                <OverlayTrigger
+	                id={this.props.tooltipId}
+                    placement={this.props.tooltipPlacement}
+                    overlay={<Tooltip>{this.props.tooltipText}</Tooltip>}
+                >
+                	{button}
+                </OverlayTrigger>
+            );
+        }
+
+        return button;
+	}
+}
